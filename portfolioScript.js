13c358d91e5f9a6146eaaab5f5ae5c7b557fcbcb
matthewtbuck <73125542+matthewtbuck@users.noreply.github.com>
2021-04-28 17:53:17 -0400

    Add files via upload

---
+++ portfolioScript.js
@@ -0,0 +1,8 @@
+var pic1=document.getElementById("beforeAndAfter");
+pic1.addEventListener("mouseover", sayName1);
+
+function sayName1() {
+	var pic1Words.getElementById("pImg1");
+	pImg1.innerHTML="Edited Outside Photo";
+}
+	
